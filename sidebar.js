e1ff7e80465593767b6d8f35787229931800f09e
degland <systemerror@hotmail.co.uk>
2021-09-13 13:48:06 +0100

    testing filtering & arraying

--- sidebar.js
+++ sidebar.js
@@ -1,28 +1,36 @@
 miro.onReady(() => {
-  // subscribe on user selected widgets
-  miro.addListener(miro.enums.event.SELECTION_UPDATED, getWidget)
-  getWidget()
+    // subscribe on user selected widgets
+    miro.addListener(miro.enums.event.SELECTION_UPDATED, generateList)
+    generateList()
 })
 
 // Get html elements for tip and text container
 const tipElement = document.getElementById('tip')
 const widgetTextElement = document.getElementById('widget-text')
 
-async function getWidget() {
-  // Get selected widgets
-  let widgets = await miro.board.selection.get()
+async function generateList() {
+    // Get selected widgets
+    let widgets = await miro.board.selection.get()
 
-  // Get first widget from selected widgets
-  let text = widgets[0].text
+    widgets = widgets.filter((widget) => widget.type === "STICKER")
 
-  // Check that widget has text field
-  if (typeof text === 'string') {
-    // hide tip and show text in sidebar
-    tipElement.style.opacity = '0'
-    widgetTextElement.value = text
-  } else {
-    // show tip and clear text in sidebar
-    tipElement.style.opacity = '1'
-    widgetTextElement.value = ''
-  }
+    let text = ""
+    
+    widgets.forEach(element => {
+        text += element.text + "\n"
+    });
+
+    
+    
+
+    // Check that widget has text field
+    if (typeof text === 'string') {
+        // hide tip and show text in sidebar
+        tipElement.style.opacity = '0'
+        widgetTextElement.value = text
+    } else {
+        // show tip and clear text in sidebar
+        tipElement.style.opacity = '1'
+        widgetTextElement.value = ''
+    }
 }
