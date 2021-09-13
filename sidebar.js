39c66347bc95a6ddef0e2cf0e394e3f174bf9f33
degland <systemerror@hotmail.co.uk>
2021-09-13 18:14:20 +0100

    added case insensitivity and early-out for stickers that have no tags

--- sidebar.js
+++ sidebar.js
@@ -8,7 +8,7 @@ miro.onReady(() => {
 const tipElement = document.getElementById('tip')
 const widgetTextElement = document.getElementById('widget-text')
 
-const version = 23
+const version = 24
 
 async function generateList() {
     let widgets = await miro.board.widgets.get()
@@ -18,58 +18,60 @@ async function generateList() {
     let text = "" + version + "\n"
     
     const html_trim_regex = new RegExp("<\/?\w+>", "g")
-    const biome_regex = new RegExp("biome:")
-    const type_regex = new RegExp("type:")
-    const subtype_regex = new RegExp("subtype:")
+    const biome_regex = new RegExp("biome:", "i")
+    const type_regex = new RegExp("type:", "i")
+    const subtype_regex = new RegExp("subtype:", "i")
 
     widgets.forEach(widget => {
-        const tags = []
-
-        widget.tags.forEach(tag => {
-            console.log(String(tag.title))
-            tags.push(String(tag.title))
-        })
-
-        text += "-----\n"
-
-        if(!tags.includes("export-ignore"))
+        if(widget.tags.length > 0)
         {
-            let title = String(widget.text).replace(html_trim_regex,"")
-            console.log(widget.text)
-            console.log("replaced + " + title)
-
-            let identified = false
-
-            let biome = "uncategorised"
-            let type = "uncategorised"
-            let subtype = "uncategorised"
-
-            //biome:1
-            //type:item
-            //subtype:crafted-tool
-
-            tags.forEach(tag => {
-                console.log(String(tag))
-                if(String(tag).match(biome_regex)){
-                    biome = tag.replace(biome_regex, "")
-                    identified = true
-                }
-                
-                else if(String(tag).match(subtype_regex)){
-                    subtype = tag.replace(subtype_regex, "")
-                    identified = true
-                }
-                else if(String(tag).match(type_regex)){
-                    type = tag.replace(type_regex, "")
-                    identified = true
-                }
-                else{ console.log("no match")}
+            const tags = []
+            widget.tags.forEach(tag => {
+                console.log(String(tag.title))
+                tags.push(String(tag.title))
             })
 
-            if(identified === true){ text += title + ", " + biome + ", " + type + ", " + subtype +",\n" }
-            else{ console.log("unidentified") }
-        }       
-    })
+            text += "-----\n"
+
+            if(!tags.includes("export-ignore"))
+            {
+                let title = String(widget.text).replace(html_trim_regex,"")
+                console.log(widget.text)
+                console.log("replaced + " + title)
+
+                let identified = false
+
+                let biome = "uncategorised"
+                let type = "uncategorised"
+                let subtype = "uncategorised"
+
+                //biome:1
+                //type:item
+                //subtype:crafted-tool
+
+                tags.forEach(tag => {
+                    console.log(String(tag))
+                    if(String(tag).match(biome_regex)){
+                        biome = tag.replace(biome_regex, "")
+                        identified = true
+                    }
+                    
+                    else if(String(tag).match(subtype_regex)){
+                        subtype = tag.replace(subtype_regex, "")
+                        identified = true
+                    }
+                    else if(String(tag).match(type_regex)){
+                        type = tag.replace(type_regex, "")
+                        identified = true
+                    }
+                    else{ console.log("no match")}
+                })
+
+                if(identified === true){ text += title + ", " + biome + ", " + type + ", " + subtype +",\n" }
+                else{ console.log("unidentified: " + title) }
+            }       
+        })
+    }
 
 
     // Check that widget has text field
